960f1a47fcb1d84139e5f552036286c63bf17b6b
Nube Colectiva <collectivecloudperu@users.noreply.github.com>
2022-09-12 13:48:09 -0500

    Update obtenerDatos.php

--- obtenerDatos.php
+++ obtenerDatos.php
@@ -1,42 +1,43 @@
 <?php
-include 'con.php';
 
-$buscarTexto = $_POST['buscadorvoz'];
+    include 'con.php';
 
-$query = 'SELECT * FROM postres WHERE nombre like "%' . $buscarTexto . '%" or precio like "%' . $buscarTexto . '%" or created_at like "%' . $buscarTexto . '%"';
+    $buscarTexto = $_POST['buscadorvoz'];
 
-$resultados = mysqli_query($con, $query);
+    $query = 'SELECT * FROM postres WHERE nombre like "%' . $buscarTexto . '%" or precio like "%' . $buscarTexto . '%" or created_at like "%' . $buscarTexto . '%"';
 
-$html = '';
-if (mysqli_num_rows($resultados) > 0)
-{
-    while ($row = mysqli_fetch_array($resultados))
+    $resultados = mysqli_query($con, $query);
+
+    $html = '';
+    if (mysqli_num_rows($resultados) > 0)
     {
-        $id = $row['id'];
-        $nombre = $row['nombre'];
-        $precio = $row['precio'];
-        $stock = $row['stock'];
-        $archivo = $row['archivo'];
-        $created_at = $row['created_at'];
+        while ($row = mysqli_fetch_array($resultados))
+        {
+            $id = $row['id'];
+            $nombre = $row['nombre'];
+            $precio = $row['precio'];
+            $stock = $row['stock'];
+            $archivo = $row['archivo'];
+            $created_at = $row['created_at'];
 
-        $html .= '<div id="datos' . $id . '">';
-        $html .= '<p>Nombre: ' . $nombre . '</p>';
-        $html .= '<p>Precio: ' . $precio . '</p>';
-        $html .= '<p>Stock: ' . $stock . '</p>';
-        $html .= '<p>Archivo: ' . $archivo . '</p>';
-        $html .= '<p>Fecha de Creación: ' . $created_at . '</p>';
-        $html .= '</div>';
+            $html .= '<div id="datos' . $id . '">';
+            $html .= '<p>Nombre: ' . $nombre . '</p>';
+            $html .= '<p>Precio: ' . $precio . '</p>';
+            $html .= '<p>Stock: ' . $stock . '</p>';
+            $html .= '<p>Archivo: ' . $archivo . '</p>';
+            $html .= '<p>Fecha de Creación: ' . $created_at . '</p>';
+            $html .= '</div>';
+        }
     }
-}
-else
-{
-    $html .= '<div >';
-    $html .= '<h2>No se encontraron registros</h2>';
-    $html .= '</div><br>';
-    $html .= '<h2>Para más desarrollos ingresa en <a href="https://nubecolectiva.com" target="_blank">nubecolectiva.com</a></h2>';
-}
+    else
+    {
+        $html .= '<div >';
+        $html .= '<h2>No se encontraron registros</h2>';
+        $html .= '</div><br>';
+        $html .= '<h2>Para más desarrollos ingresa en <a href="https://nubecolectiva.com" target="_blank">nubecolectiva.com</a></h2>';
+    }
 
-echo $html;
+    echo $html;
 
-exit;
+    exit;
 
